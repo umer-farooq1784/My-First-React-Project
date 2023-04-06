5906b0df470b2190b9de25841dfad145e09c90d2
umer-farooq1784 <umerfarroq987@gmail.com>
2023-04-06 21:55:15 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [priority, setPriority] = useState(1);
+
+  const addItem = () => {
+    const newItem = {
+      id: items.length > 0 ? items[items.length - 1].id + 1 : 1,
+      name: inputValue.trim(),
+      priority: priority
+    };
+  
+    if (newItem.name !== '') {
+      setItems(prevItems => [...prevItems, newItem]);
+      setInputValue('');
+      setPriority(1);
+    }
+  };
+
+  const removeItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  const updatePriority = (id, newPriority) => {
+    setItems(
+      items.map((item) => {
+        if (item.id === id) {
+          if(newPriority <= 3 && newPriority > 0)
+          {
+            return { ...item, priority: newPriority };
+          }
+          else
+          {
+            return item;
+          }
+          
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+
+  const moveItemToTop = (id) => {
+    const index = items.findIndex((item) => item.id === id);
+    const item = items[index];
+    setItems([item, ...items.filter((item, i) => i !== index)]);
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handlePriorityChange = (e) => {
+    setPriority(Number(e.target.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>My Wish List</h1>
+      <hr />
+      <br />
+      <div className='innerDiv'>
+        <label><strong>Add item to the WISH LIST</strong></label>
+        <input type="text" value={inputValue} onChange={handleInputChange} placeholder='Enter Item'/>
+        <select value={priority} onChange={handlePriorityChange}>
+          <option value="1">Low</option>
+          <option value="2">Medium</option>
+          <option value="3">High</option>
+        </select>
+        <button onClick={addItem}>Add Item</button>
+      </div>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <h4>{item.name} - {item.priority}</h4>
+            <button onClick={() => removeItem(item.id)}>Remove</button>
+            <button onClick={() => updatePriority(item.id, item.priority + 1)}>Increase Priority</button>
+            <button onClick={() => updatePriority(item.id, item.priority - 1)}>Decrease Priority</button>
+            <button onClick={() => moveItemToTop(item.id)}>Move to Top</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
